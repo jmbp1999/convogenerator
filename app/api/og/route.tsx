e1e3576869d1b6598cb16ea9a2173ed3ba918b2d
jmbp1999 <pamudithaj98@gmail.com>
2024-04-26 22:56:21 +0530

    jhj

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -2,7 +2,7 @@ import ImageGenerator from "@/app/components/Image";
 import { ImageResponse } from "next/og";
 
 
-export const runtime = "node";
+export const runtime = "edge";
 
 
 export async function GET(request: Request) {
@@ -10,9 +10,7 @@ export async function GET(request: Request) {
         const data = await fetch("https://o7wc2ghhopwh7yac3an4sy6nma0ptyhu.lambda-url.us-east-1.on.aws");
         const bot = await data.json();
         const botImageUri = bot.liveVersion.profileUrl; // Replace 'imageUri' with the actual property name for the image URI
-        return new ImageResponse(
-                <ImageGenerator profileUrl={botImageUri} />,
-                );
+        return new ImageResponse(<ImageGenerator profileUrl={botImageUri} />);
 
     } catch (e: any) {
         return new Response("Error: " + e.message, { status: 500 });
--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -6,36 +6,42 @@ export default function ImageGenerator({ profileUrl }: { profileUrl: string }) {
         height: "100%",
         width: "100%",
         display: "flex",
-        textAlign: "center",
-        alignItems: "center",
-        justifyContent: "center",
         flexDirection: "column",
-        flexWrap: "nowrap",
-        backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
+        opacity: 1,
+        background: "linear-gradient(270.83deg, #FF7425 0%, #BA57E9 10.66%, #005AFF 100%)"
+
       }}
     >
       <div
         style={{
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          padding:"16px 32px 0 32px",
         }}
+
       >
-        <img src={profileUrl} alt="Bot" style={{ height: "500px", margin: "0 75px" }} />
+        <h1>ConvoGrid</h1>
+        
       </div>
+      <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
       <div
         style={{
           display: "flex",
-          fontSize: 40,
-          fontStyle: "normal",
-          color: "black",
-          marginTop: 30,
-          lineHeight: 1.8,
-          whiteSpace: "pre-wrap",
+          flexDirection: "column",
+          justifyContent:"center",
+          alignItems:"center",
+          opacity:0.3,
+          border: "4px solid rgba(255, 255, 255, 1)", // Border color with opacity 1
+          backgroundColor:"white",
+
+          padding:"16px 32px 16px 32px",
+          borderRadius:"16px"
         }}
       >
-        <b>Convogrid</b>
+        <img src={profileUrl} alt="Bot" style={{ height: "250px",width:"250px" ,opacity:"1 !important",borderRadius:"16px"}} />
+        <p style={{fontStyle:"bold",fontSize:"32px",fontWeight:"1200",color:"white",opacity:"1 !important"}}> Andrew Huberman</p>
       </div>
+      </div>
+
     </div>
   );
 }
