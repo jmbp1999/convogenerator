1f6b127b6708810f33afc69604c96769aaf7db5d
jmbp1999 <pamudithaj98@gmail.com>
2024-04-26 23:31:34 +0530

    image

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -1,4 +1,3 @@
-
 export default function ImageGenerator({ profileUrl }: { profileUrl: string }) {
   return (
     <div
@@ -8,51 +7,48 @@ export default function ImageGenerator({ profileUrl }: { profileUrl: string }) {
         display: "flex",
         flexDirection: "column",
         opacity: 1,
-        background: "linear-gradient(270.83deg, #FF7425 0%, #BA57E9 10.66%, #005AFF 100%)"
-
+        background: "linear-gradient(270.83deg, #FF7425 0%, #BA57E9 10.66%, #005AFF 100%)",
       }}
     >
       <div
         style={{
           display: "flex",
-          padding:"16px 32px 0 60px",
-          
+          padding: "16px 32px 0 60px",
         }}
-
       >
-        <div style={{
-          display: "flex",
-          backgroundColor:"white",
-          alignItems:"center",
-          padding:"0px 32px 0px 32px",
-          borderRadius:"24px 20px 52px 24px",
-        }}> 
-        <img src="https://intervee.s3.amazonaws.com/Group+36764.png" height={"50px"} width={"80px"}></img>
-                <h1 style={{fontWeight:"800"}}>ConvoGrid</h1>
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "white",
+            alignItems: "center",
+            padding: "0px 32px 0px 32px",
+            borderRadius: "20px 20px 42px 20px",
+          }}
+        >
+          <img src="https://intervee.s3.amazonaws.com/Group+36764.png" height={"50px"} width={"80px"}></img>
+          <h1 style={{ fontWeight: "800" }}>ConvoGrid</h1>
         </div>
-
-        
       </div>
-      <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent:"center",
-          alignItems:"center",
-          opacity:0.3,
-          border: "4px solid rgba(255, 255, 255, 1)", // Border color with opacity 1
-          backgroundColor:"white",
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            opacity: 0.3,
+            border: "4px solid rgba(255, 255, 255, 1)", // Border color with opacity 1
+            backgroundColor: "white",
+            marginTop:"54px",
 
-          padding:"16px 32px 16px 32px",
-          borderRadius:"16px"
-        }}
-      >
-        <img src={profileUrl} alt="Bot" style={{ height: "250px",width:"250px" ,opacity:"1 !important",borderRadius:"16px"}} />
-        <p style={{fontStyle:"bold",fontSize:"32px",fontWeight:"1200",color:"white",opacity:"1 !important"}}> Andrew Huberman</p>
+            padding: "16px 32px 16px 32px",
+            borderRadius: "16px",
+          }}
+        >
+          <img src={profileUrl} alt="Bot" style={{ height: "250px", width: "250px", opacity: "1 !important", borderRadius: "16px" }} />
+          <p style={{ fontStyle: "bold", fontSize: "32px", fontWeight: "1200", color: "white", opacity: "1 !important" }}> Andrew Huberman</p>
+        </div>
       </div>
-      </div>
-
     </div>
   );
 }
