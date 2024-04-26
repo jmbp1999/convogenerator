7bf0c28e8a65358f7a2e4bf8041be9505a504bc5
jmbp1999 <pamudithaj98@gmail.com>
2024-04-26 17:09:43 +0530

    ss

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -8,48 +8,47 @@ export async function GET(request:Request){
     try {
         return new ImageResponse(<div
             style={{
-              display: 'flex',
               height: '100%',
               width: '100%',
+              display: 'flex',
+              textAlign: 'center',
               alignItems: 'center',
               justifyContent: 'center',
               flexDirection: 'column',
-              backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
-              fontSize: 60,
-              letterSpacing: -2,
-              fontWeight: 700,
-              textAlign: 'center',
+              flexWrap: 'nowrap',
+              backgroundColor: 'white',
+              backgroundImage: 'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+              backgroundSize: '100px 100px',
             }}
-            >
+          >
             <div
               style={{
-                backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
-                backgroundClip: 'text',
-                '-webkit-background-clip': 'text',
-                color: 'transparent',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
-              Develop
+              <svg
+                height={80}
+                viewBox="0 0 75 65"
+                fill="black"
+                style={{ margin: '0 75px' }}
+              >
+                <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
+              </svg>
             </div>
             <div
               style={{
-                backgroundImage: 'linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))',
-                backgroundClip: 'text',
-                '-webkit-background-clip': 'text',
-                color: 'transparent',
+                display: 'flex',
+                fontSize: 40,
+                fontStyle: 'normal',
+                color: 'black',
+                marginTop: 30,
+                lineHeight: 1.8,
+                whiteSpace: 'pre-wrap',
               }}
             >
-              Preview
-            </div>
-            <div
-              style={{
-                backgroundImage: 'linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))',
-                backgroundClip: 'text',
-                '-webkit-background-clip': 'text',
-                color: 'transparent',
-              }}
-            >
-              Ship
+              <b>Vercel Edge Network</b>
             </div>
           </div>
           )
